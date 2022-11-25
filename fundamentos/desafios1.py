525b234d64de75c3eb0dfbfe73b94a7f4748abf4
MatheusCostaLara-dev <matheuscostalara@hotmail.com>
2022-11-25 12:19:00 -0300

    adicionando os primeiros exercicios

---
+++ fundamentos/desafios1.py
@@ -0,0 +1,27 @@
+#  Desafio 1: Crie um Script
+#  que leia o nome de uma pessoa e
+#  mostre uma mensagem de boas-vindas
+#  de acordo com o valor digitado.
+
+print ('=========== DESAFIO 01 ============')
+nome = input('Qual o seu nome?: ')
+print('Olá,', nome, '! Prazer em te conhecer!')
+
+#  Desafio 2: Crie um script
+#  que leia o dia, o mês e o ano de nascimento
+#  de uma pessoa e mostre uma mensagem
+#  com a data formatada.
+
+print ('=========== DESAFIO 02 ============')
+dia = input('dia = ')
+mes = input('mês = ')
+ano = input('ano = ')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano, '. Correto?')
+
+#  Desafio 3: Crie um script Python
+#  que leia dois numeros e tente mostrar a soma entre eles
+
+print ('=========== DESAFIO 03 ============')
+primeiroNumero = int(input('primeiro número:'))
+segundoNumero = int(input('segundo número:'))
+print('A soma é', primeiroNumero + segundoNumero)
---
+++ fundamentos/olaMundo.py
@@ -0,0 +1,13 @@
+print('Estou aprendendo Python')
+print(2 + 5)
+print('olá, Mundo!')
+print('7'+'4')
+# print('Olá' + 5) isso dá errado, apesar do uso 
+# "correto da concatenação". O uso correto é com vírgula(,)
+print('Olá', 5)
+# input é uma função que permite o usuário escrever
+nome = input('Qual o seu nome?: ')
+idade = input('Quantos anos você tem?: ')
+peso = input('Quanto você pesa?: ')
+
+print(nome, idade, peso)
